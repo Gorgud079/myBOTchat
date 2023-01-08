51cd191003625880f8b10c8b767adb70f5158515
Gorgud079 <des079@inbox.ru>
2023-01-09 00:41:55 +0400

    change cod, add some functions (separated the code for clarity)

--- myBOTchat.py
+++ myBOTchat.py
@@ -9,17 +9,17 @@ first_number = 0
 last_number = 0
 start_point = "rus"
 bot = telebot.TeleBot(TOKEN)
-valut = {
-	"rub" : {
-		"dollars": 0.01,
-		"euro" : 0.01},
-	"euro" : {
-		"dollars": 1.08,
-		"rub": 75.66},
-	"dollars": {
-		"euro": 0.93,
-		"rub": 70.34}
-}
+# valut = {
+# 	"rub" : {
+# 		"dollars": 0.01,
+# 		"euro" : 0.01},
+# 	"euro" : {
+# 		"dollars": 1.08,
+# 		"rub": 75.66},
+# 	"dollars": {
+# 		"euro": 0.93,
+# 		"rub": 70.34}
+# }
 # r = requests.get("https://www.banki.ru/products/currency/cb/")
 # print(dir(r))
 # with open("index.html", "w") as file:
@@ -40,7 +40,6 @@ def start_welcome(message):
 def callback_worker(call):
 	try:
 		if call.data == "start":
-			# bot.send_message(call.message.chat.id, "Выберите валюту с которой нужно перевести")
 			keyboard = types.InlineKeyboardMarkup()
 			rub_start_key = types.InlineKeyboardButton(text="RUB", callback_data="rub")
 			keyboard.add(rub_start_key)
@@ -48,16 +47,8 @@ def callback_worker(call):
 			keyboard.add(eu_start_key)
 			dollars_start_key = types.InlineKeyboardButton(text="DOLLARS", callback_data="dollars")
 			keyboard.add(dollars_start_key)
-
 			bot.send_message(call.message.chat.id, text="Выберите валюту с которой нужно перевести.", reply_markup=keyboard)
-			# bot.register_next_step_handler(call.message, start_choose)
-			# if call.data == "rus":
-			# 	bot.send_message(message.from_user.id, "")
-			# 	bot.register_next_step_handler(call.message, start_rus)
-			# bot.register_next_step_handler(call.message, start)
 		elif call.data == "help":
-			# bot.send_message(call.message.chat.id, "Я бот-конвертер, провожу обмен денежной единицы одной страны на деньги другой по текущему курсу. Доступны: евро, доллары и рубли.")
-			# bot.register_next_step_handler(call.message, start_welcome)
 			keyboard = types.InlineKeyboardMarkup()
 			key_start = types.InlineKeyboardButton(text="ДА", callback_data="start")
 			keyboard.add(key_start)
@@ -65,15 +56,37 @@ def callback_worker(call):
 			keyboard.add(key_help)
 			info = "Я бот-конвертер, провожу обмен денежной единицы одной страны на деньги другой по текущему курсу. Доступны: евро, доллары и рубли. Продолжим?"
 			bot.send_message(call.message.chat.id, text=info, reply_markup=keyboard)
-		elif call.data == "rub" or call.data == "dollars":
-			bot.send_message(call.message.chat.id, text="Выберите")
-			bot.register_next_step_handler(call.message, start_choose)
-		elif call.data == "euro":
-			bot.send_message(call.message.chat.id, "Выберите 1hjhj")
-
+		elif call.data == "rub" or call.data == "euro" or call.data == "dollars":
+			x = second_choose(call)
+			start_point = call
+			return call, x, start_point
 	except KeyError:
 		pass
 
+
+# @bot.callback_query_handler(func=lambda call: True)
+def second_choose(call):
+	try:
+		if call.data == "rub" or call.data == "euro" or call.data == "dollars":
+			keyboard = types.InlineKeyboardMarkup()
+			rub_last_key = types.InlineKeyboardButton(text="RUB", callback_data="rub_last")
+			keyboard.add(rub_last_key)
+			eu_last_key = types.InlineKeyboardButton(text="EURO", callback_data="euro_last")
+			keyboard.add(eu_last_key)
+			dollars_last_key = types.InlineKeyboardButton(text="DOLLARS", callback_data="dollars_last")
+			keyboard.add(dollars_last_key)
+			bot.send_message(call.message.chat.id, text="Выберите на какую валюту поменять", reply_markup=keyboard)
+			last_point = call
+			number = price(call)
+			return call, number, last_point
+
+
+
+	except KeyError:
+		pass
+def price(call):
+	if call.data == "rub_l" or call.data == "euro1" or call.data == "dollars2":
+			bot.send_message(call.message.chat.id, "Введите сумму")
 # @bot.callback_query_handler(func=lambda call:True)
 # def callback_work(call):
 # 	try:
@@ -86,16 +99,36 @@ def callback_worker(call):
 # 		pass
 
 def start_choose(call):
-	if call.data == "rub":
-		global start_point
-		start_point = call.text
-		bot.send_message(call.message.chat.id, "Введите rub")
-		bot.register_next_step_handler(call.message, rub_start)
-		"""НЕ РАБОТАЕТ"""
-	elif call.data == "euro":
-		bot.register_next_step_handler(call.message, eu_start)
-	elif call.data == "dollars":
-		bot.register_next_step_handler(call.message, dollars_start)
+	keyboard = types.InlineKeyboardMarkup()
+	key_start = types.InlineKeyboardButton(text="ДА", callback_data="start")
+	keyboard.add(key_start)
+	key_help = types.InlineKeyboardButton(text="НЕ ЗНАЮ", callback_data="help")
+	keyboard.add(key_help)
+	info = "Я бот-конвертер, провожу обмен денежной единицы одной страны на деньги другой по текущему курсу. Доступны: евро, доллары и рубли. Продолжим?"
+	bot.send_message(call.message.chat.id, text=info, reply_markup=keyboard)
+
+
+
+		# bot.send_message(call.message, "Whatever")
+		# keyboard = types.InlineKeyboardMarkup()
+		# key_start = types.InlineKeyboardButton(text="START11", callback_data="start1")
+		# keyboard.add(key_start)
+		# key_help = types.InlineKeyboardButton(text="HELP11", callback_data="help11")
+		# keyboard.add(key_help)
+		# info = "Second number"
+		# bot.send_message(call.message, text=info, reply_markup=keyboard)
+
+
+	# if call.data == "rub":
+	# 	global start_point
+	# 	start_point = call.text
+	# 	bot.send_message(call.message.chat.id, "Введите rub")
+	# 	bot.register_next_step_handler(call.message, rub_start)
+	# 	"""НЕ РАБОТАЕТ"""
+	# elif call.data == "euro":
+	# 	bot.register_next_step_handler(call.message, eu_start)
+	# elif call.data == "dollars":
+	# 	bot.register_next_step_handler(call.message, dollars_start)
 
 
 def rub_start(message):
